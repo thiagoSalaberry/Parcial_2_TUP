9b8ec0d20f5557953e13f2f344cbd25d42fdfcf3
Thiago <thiagosalaberry99@gmail.com>
2025-06-23 09:29:26 -0300

    Pantallas init

---
+++ pantallas_modelo.py
@@ -0,0 +1,20 @@
+pant_inicio = [
+    { "tipo": "texto", "valor": "CODY CROSS", "pos": (100, 100) },
+    { "tipo": "boton", "valor": "Jugar", "pos": (100, 100), "callback": lambda: print("Jugar") },
+    { "tipo": "texto", "valor": "CODY CROSS", "pos": (100, 100) },
+]
+pant_jugar = [
+    { "tipo": "texto", "valor": "CODY CROSS", "pos": (100, 100) },
+    { "tipo": "boton", "valor": "Jugar", "pos": (100, 100), "callback": lambda: print("Jugar") },
+    { "tipo": "texto", "valor": "CODY CROSS", "pos": (100, 100) },
+]
+pant_estadisticas = [
+    { "tipo": "texto", "valor": "CODY CROSS", "pos": (100, 100) },
+    { "tipo": "boton", "valor": "Jugar", "pos": (100, 100), "callback": lambda: print("Jugar") },
+    { "tipo": "texto", "valor": "CODY CROSS", "pos": (100, 100) },
+]
+pant_creditos = [
+    { "tipo": "texto", "valor": "CODY CROSS", "pos": (100, 100) },
+    { "tipo": "boton", "valor": "Jugar", "pos": (100, 100), "callback": lambda: print("Jugar") },
+    { "tipo": "texto", "valor": "CODY CROSS", "pos": (100, 100) },
+]
